03b76d8eb67d363c9d4f79185f9048009ac3d83a
Mostafa Amininasab <m.amininasab@alibaba.ir>
2021-03-23 00:58:18 +0430

    fix: eslint config

--- dist/bundle.esm.js
+++ dist/bundle.esm.js
@@ -1,22 +1,141 @@
-var DEFAULT_SIZE = 32;
-function createGear() {
-  var size = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : DEFAULT_SIZE;
-  var cursor = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
 
-  var _size = size || DEFAULT_SIZE;
+  return obj;
+}
 
-  var _cursor = cursor > 0 ? cursor % _size : (_size - Math.abs(cursor) % _size) % _size;
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
 
-  if (_size < 2) {
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    if (enumerableOnly) symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    });
+    keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+
+    if (i % 2) {
+      ownKeys(Object(source), true).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+
+  return target;
+}
+
+function _toConsumableArray(arr) {
+  return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();
+}
+
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) return _arrayLikeToArray(arr);
+}
+
+function _iterableToArray(iter) {
+  if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter);
+}
+
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+  return arr2;
+}
+
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+
+var DEFAULT_OPTIONS = {
+  period: 32,
+  oscillators: 7,
+  initialState: null
+};
+function epicles() {
+  var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+  var mergedOptions = _objectSpread2(_objectSpread2({}, DEFAULT_OPTIONS), options);
+
+  var period = mergedOptions.period,
+      oscillators = mergedOptions.oscillators,
+      initialState = mergedOptions.initialState;
+
+  if (period < 2) {
     throw new Error('gear size must be at least 2');
   }
 
+  if (initialState && initialState.length !== period) {
+    throw new Error('initial state length must be equal to period');
+  }
+
+  var state = initialState ? sanetizeState(initialState) : Array(oscillators).fill(0);
   var subscribers = new Map();
 
+  function sanetizeState(state) {
+    return state.map(function (oscillator) {
+      return oscillator > 0 ? oscillator % period : (period - Math.abs(oscillator) % period) % period;
+    });
+  }
+
+  function getState() {
+    return _toConsumableArray(state);
+  }
+
+  function cyclicIncrement(value) {
+    return (value + 1) % period;
+  }
+
+  function increment(state, index) {
+    var nextState = _toConsumableArray(state);
+
+    nextState[index] = cyclicIncrement(nextState[index]);
+
+    if (nextState[index] === 0 && index < state.length - 1) {
+      return increment(nextState, index + 1);
+    }
+
+    return nextState;
+  }
+
   function tick() {
-    _cursor = (_cursor + 1) % _size;
+    state = increment(state, 0);
+    var emitState = getState();
     subscribers.forEach(function (subscriber) {
-      subscriber(_cursor);
+      subscriber(emitState);
     });
   }
 
@@ -37,4 +156,4 @@ function createGear() {
   };
 }
 
-export { createGear };
+export { epicles };
--- dist/bundle.umd.js
+++ dist/bundle.umd.js
@@ -1,50 +1,169 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.Epicles = {}));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.Epicles = {}));
 }(this, (function (exports) { 'use strict';
 
-    var DEFAULT_SIZE = 32;
-    function createGear() {
-      var size = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : DEFAULT_SIZE;
-      var cursor = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-
-      var _size = size || DEFAULT_SIZE;
-
-      var _cursor = cursor > 0 ? cursor % _size : (_size - Math.abs(cursor) % _size) % _size;
-
-      if (_size < 2) {
-        throw new Error('gear size must be at least 2');
-      }
-
-      var subscribers = new Map();
-
-      function tick() {
-        _cursor = (_cursor + 1) % _size;
-        subscribers.forEach(function (subscriber) {
-          subscriber(_cursor);
-        });
-      }
-
-      function subscribe(callback) {
-        var key = Symbol();
-        subscribers.set(key, callback);
-
-        var unsubscribe = function unsubscribe() {
-          subscribers["delete"](key);
-        };
-
-        return unsubscribe;
-      }
-
-      return {
-        tick: tick,
-        subscribe: subscribe
-      };
+  function _defineProperty(obj, key, value) {
+    if (key in obj) {
+      Object.defineProperty(obj, key, {
+        value: value,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      });
+    } else {
+      obj[key] = value;
     }
 
-    exports.createGear = createGear;
+    return obj;
+  }
 
-    Object.defineProperty(exports, '__esModule', { value: true });
+  function ownKeys(object, enumerableOnly) {
+    var keys = Object.keys(object);
+
+    if (Object.getOwnPropertySymbols) {
+      var symbols = Object.getOwnPropertySymbols(object);
+      if (enumerableOnly) symbols = symbols.filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+      });
+      keys.push.apply(keys, symbols);
+    }
+
+    return keys;
+  }
+
+  function _objectSpread2(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i] != null ? arguments[i] : {};
+
+      if (i % 2) {
+        ownKeys(Object(source), true).forEach(function (key) {
+          _defineProperty(target, key, source[key]);
+        });
+      } else if (Object.getOwnPropertyDescriptors) {
+        Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+      } else {
+        ownKeys(Object(source)).forEach(function (key) {
+          Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+        });
+      }
+    }
+
+    return target;
+  }
+
+  function _toConsumableArray(arr) {
+    return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();
+  }
+
+  function _arrayWithoutHoles(arr) {
+    if (Array.isArray(arr)) return _arrayLikeToArray(arr);
+  }
+
+  function _iterableToArray(iter) {
+    if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter);
+  }
+
+  function _unsupportedIterableToArray(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(o);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+  }
+
+  function _arrayLikeToArray(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+
+    for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+    return arr2;
+  }
+
+  function _nonIterableSpread() {
+    throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+  }
+
+  var DEFAULT_OPTIONS = {
+    period: 32,
+    oscillators: 7,
+    initialState: null
+  };
+  function epicles() {
+    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    var mergedOptions = _objectSpread2(_objectSpread2({}, DEFAULT_OPTIONS), options);
+
+    var period = mergedOptions.period,
+        oscillators = mergedOptions.oscillators,
+        initialState = mergedOptions.initialState;
+
+    if (period < 2) {
+      throw new Error('gear size must be at least 2');
+    }
+
+    if (initialState && initialState.length !== period) {
+      throw new Error('initial state length must be equal to period');
+    }
+
+    var state = initialState ? sanetizeState(initialState) : Array(oscillators).fill(0);
+    var subscribers = new Map();
+
+    function sanetizeState(state) {
+      return state.map(function (oscillator) {
+        return oscillator > 0 ? oscillator % period : (period - Math.abs(oscillator) % period) % period;
+      });
+    }
+
+    function getState() {
+      return _toConsumableArray(state);
+    }
+
+    function cyclicIncrement(value) {
+      return (value + 1) % period;
+    }
+
+    function increment(state, index) {
+      var nextState = _toConsumableArray(state);
+
+      nextState[index] = cyclicIncrement(nextState[index]);
+
+      if (nextState[index] === 0 && index < state.length - 1) {
+        return increment(nextState, index + 1);
+      }
+
+      return nextState;
+    }
+
+    function tick() {
+      state = increment(state, 0);
+      var emitState = getState();
+      subscribers.forEach(function (subscriber) {
+        subscriber(emitState);
+      });
+    }
+
+    function subscribe(callback) {
+      var key = Symbol();
+      subscribers.set(key, callback);
+
+      var unsubscribe = function unsubscribe() {
+        subscribers["delete"](key);
+      };
+
+      return unsubscribe;
+    }
+
+    return {
+      tick: tick,
+      subscribe: subscribe
+    };
+  }
+
+  exports.epicles = epicles;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
